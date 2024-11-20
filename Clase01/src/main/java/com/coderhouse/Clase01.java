999ae296bad28cc82e47c737ce4023b5d8d8df7b
“mariapiagrosso” <mariapiagrosso@gmail.com>
2024-11-20 20:04:47 -0300

    Clase 02

--- Clase01/src/main/java/com/coderhouse/Clase01.java
+++ Clase01/src/main/java/com/coderhouse/Clase01.java
@@ -3,7 +3,75 @@ package com.coderhouse;
 public class Clase01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+//		System.out.println("Hola Coders");
+//		 
+		//Declaracion de Variables
+		
+//		byte variableA = 100;
+//		double variableConDecimales = -1000.50d;
+//		float variableFloat = 13468.66f;
+//		char miPrimerChar = 'M';
+//		String miPrimerString = "Mi Primer String !!";
+//		boolean	variableBoolean = false;
+//		long numeroLargo = 16546354687316874L; 
+//		String saludoInicial = "Hola Coders desde un string";
+//		String susanaMood = "Dame Plata";
+//		
+//		System.out.println(saludoInicial);
+//		System.out.println(numeroLargo);
+//		System.out.println(saludoInicial + ' ' + variableA);
+//		System.out.println(susanaMood);
+		
+//		double numeroUno = 100d;
+//		double numeroDos = 50d;
+//		int numeroTres = 12;
+//		int numeroCuatro = 4;
+//		int numeroCinco = 5;
+//		
+//		double resultadoSuma = numeroDos + numeroUno;
+//		double resultadoresta = numeroDos - numeroUno;
+//		double resultadoMulti = numeroDos * numeroUno;
+//		double resultadoDiv = numeroDos / numeroUno;
+//		int resultadoModuloA = numeroTres % numeroCuatro;
+//		int resultadoModuloB = numeroTres % numeroCinco;
+//		
+//		
+//		
+//		System.out.println(numeroDos + numeroUno );
+//		
+//		System.out.println(resultadoSuma);
+//		System.out.println(resultadoresta);
+//		System.out.println(resultadoMulti);
+//		System.out.println(resultadoDiv);
+//		System.out.println("El resultado del Modulo A es: " + resultadoModuloA);
+//		System.out.println("El resultado del Modulo B es: " + resultadoModuloB);
+//		
+		//Incrementar
+		
+//		int contador = 0; //Definiendo e inicializando 
+//		
+//		System.out.println(contador);
+//		contador = 10; //Reasignar Valor
+//		System.out.println(contador);
+//		contador++;
+//		System.out.println(contador);
+//		contador--;
+//		System.out.println(contador);
+		
+		int numeroA = 10;
+		int numeroB = 11;
+		
+		System.out.println(numeroA == numeroB); //Son iguales?
+		System.out.println(numeroA != numeroB); //Son distintos?
+		System.out.println(numeroA < numeroB); //A es MAYOR a B?
+		System.out.println(numeroA > numeroB); //A es MENOR a B?
+		System.out.println(numeroA <= numeroB); //A es MAYOR O IGUAL a B?
+		System.out.println(numeroA >= numeroB); //A es MENOR O IGUAL a B?
+
+		
+		
+		
 		
 
 	}
