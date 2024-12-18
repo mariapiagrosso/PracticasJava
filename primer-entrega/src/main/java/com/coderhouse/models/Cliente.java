38d7bbb8596f424d8ae9a0cc8db834ff756bb920
“mariapiagrosso” <mariapiagrosso@gmail.com>
2024-12-18 02:49:57 -0300

    Entrega

--- primer-entrega/src/main/java/com/coderhouse/models/Cliente.java
+++ primer-entrega/src/main/java/com/coderhouse/models/Cliente.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
